cdc73a25c0f90a19e282bf7c84ebeeaac8ad9fc6
y3e <THAYEEBOI890@GMAIL.COM>
2024-11-29 10:33:36 -0600

    remove build

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appexelentos-welcome_FILE /home/y3e/exelentos-welcome/build/appexelentos-welcome)
-set(__QT_DEPLOY_TARGET_appexelentos-welcome_TYPE EXECUTABLE)
--- build/CMakeFiles/appexelentos-welcome.dir/build/.qt/rcc/qrc_qmake_exelentos-welcome.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/appexelentos-welcome.dir/build/.qt/rcc/qrc_qmake_exelentos-welcome.cpp.o: \
- /home/y3e/exelentos-welcome/build/.qt/rcc/qrc_qmake_exelentos-welcome.cpp \
- /usr/include/stdc-predef.h
